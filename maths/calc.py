66b7505c2d0aefb4c7bbd85ff5bec0cdfddbcb24
Charles ukueje <chtreasure@gmail.com>
2019-11-16 14:54:14 -0500

    maths_string

--- maths/calc.py
+++ maths/calc.py
@@ -1,3 +1,21 @@
 def subtraction(num1, num2):
     return num1 - num2
+
+def addition(num1, num2):
+    return num1 + num2
+
+def multiplication(num1,num2):
+    return num1*num2
+
+def square_root(num1):
+    return num1**(1/2)
+
+def exponential(num1, num2):
+    return num1 ** num2
+
+def division(num1,num2):
+    return num1 / num2
+
+def multiplication(num1, num2):
+    return num1 * num2
     
