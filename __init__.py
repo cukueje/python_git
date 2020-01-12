23fa47b169f43073d50eaf9d2c2ec7a60849c8b7
Charles ukueje <chtreasure@gmail.com>
2020-01-11 20:56:44 -0500

    class

---
+++ __init__.py
@@ -0,0 +1,26 @@
+class Cinema:
+    def __init__(
+      self, 
+      movie_code, 
+      movie_name, 
+      movie_category, 
+      movie_schedule, 
+      theatre_list, 
+      running_time,):
+        self.movie_name = movie_name
+        self.movie_code = movie_code
+        self.movie_category = movie_category
+        self.running_time = running_time
+    def movie_code(self, code):
+        return self.code
+    def movie_name(slef, name):
+        return self.name
+    def movie_category(self, category):
+        return self.category
+    def movie_schedule(self, schedule):
+        return self.schedule
+    def running_time(self, time):
+        return self.time
+    def theatre_list(self, theatre):
+        return self.theatre
+    
